9a457ef955f50b7f8ba5de0751a61b67113160f5
yasuaki640 <yasuaki640@gmail.com>
2022-07-07 11:26:55 +0900

    inline styles

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
-import { useState, useCallback, useMemo } from 'react';
-import { ChildArea } from './ChildArea';
 import './styles.css';
+import { InlineStyle } from './components/InlineStyle';
 
 export default function App() {
-  console.log('app');
-
-  const [text, setText] = useState('');
-  const onChangeText = (e) => setText(e.target.value);
-
-  const [open, setOpen] = useState(false);
-  const onClickOpen = () => setOpen(!open);
-
-  const onClickClose = useCallback(() => setOpen(false), []);
-
-  // 変数自体のメモ化
-  const temp = useMemo(() => 1 + 3, []);
-  console.log(temp);
-
   return (
     <div className='App'>
-      <input value={text} onChange={onChangeText} />
-      <br />
-      <br />
-      <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} onClickClose={onClickClose} />
+      <InlineStyle />
     </div>
   );
 }
